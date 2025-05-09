198fbfae1a1bb185143b2fe442aec9261a59cf40
Leandro Aquino <00036725@uca.edu.sv>
2025-05-08 21:21:26 -0600

    update :white_check_mark:

--- Tema1_If,Else,Else-If/15Multa.cpp
+++ Tema1_If,Else,Else-If/15Multa.cpp
@@ -6,7 +6,8 @@ int main(){
 
     cout <<"Enter the speed at which you were driving: ";
     cin >>SPEED;
-
+    //se ordena de la mayor primero ya que se evalua primero en >=80 y al no entrar ahi pasa al siguiente
+    // si es alrevez no funcionaria 
     if(SPEED >= 80){
         cout <<"You will be fined more for speeding";
     }else if( SPEED >= 60){
